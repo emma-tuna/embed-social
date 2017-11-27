e146c777296f99c6726c1d29761c0ae77add3f22
Emma Edgar <emma.edgar@tunatraffic.com>
2017-11-27 12:37:26 -0600

    add basic functionality for popup

--- es-shortcode.php
+++ es-shortcode.php
@@ -47,3 +47,4 @@ if ( ! shortcode_exists( 'es-embed' ) ) {
 
 
 }
+
--- js/es-mce.js
+++ js/es-mce.js
@@ -5,22 +5,137 @@
 (function() {
     /* Register the buttons */
     tinymce.create('tinymce.plugins.EsButtons', {
-        init : function(ed, url) {
+        init : function(editor, url) {
             /**
              * Inserts shortcode content
              */
-            ed.addButton( 'button_es_embed', {
-                title : 'Insert Embed Social Shortcode',
+            editor.addButton('button_es_embed', {
                 image : url + '/social-embed-icon.png',
-                onclick : function() {
-                    ed.selection.setContent('[es-embed url= size=small align=center description=true]');
+                tooltip: 'BootStrap Panel',
+                onclick: function() {
+                    editor.execCommand('bs3_panel_popup','',{
+                        embedUrl : ''
+                    });
                 }
+            });
+
+            // Called when we click the Insert Gistpen button
+            editor.addCommand( 'bs3_panel_popup', function(ui, v) {
+
+                // Defaults
+                var embedUrl = '';
+                if (v.embedUrl)
+                    embedUrl = v.embedUrl;
+                var embedAlign = '';
+                if (v.embedAlign)
+                    embedAlign = v.embedAlign;
+                var embedSize = '';
+                if (v.embedSize)
+                    embedSize = v.embedSize;
+                var embedDesc = '';
+                if (v.embedDesc)
+                    embedDesc = v.embedDesc;
+
+
+
+                // Calls the pop-up modal
+                editor.windowManager.open({
+                    // Modal settings
+                    title: 'Insert Shortcode',
+                    width: jQuery( window ).width() * 0.7,
+                    // minus head and foot of dialog box
+                    height: (jQuery( window ).height() - 36 - 50) * 0.7,
+                    inline: 1,
+                    id: 'plugin-slug-insert-dialog',
+                    body: [
+                        {//url
+                            type: 'textbox',
+                            name: 'embedUrl',
+                            label: 'URL',
+                            value: embedUrl
+                        },
+                        // alignment
+                        {
+                            type: 'listbox',
+                            name: 'embedAlign',
+                            label: 'Alignment',
+                            value: embedAlign,
+                            'values': [
+                                {text: 'Left', value: 'left'},
+                                {text: 'Center', value: 'center'},
+                                {text: 'Right', value: 'right'}
+                            ],
+                            tooltip: 'Select the type of panel you want'
+                        },
+                        //embedSize
+                        {
+                            type: 'listbox',
+                            name: 'embedSize',
+                            label: 'Size',
+                            value: embedSize,
+                            'values': [
+                                {text: 'Small', value: 'small'},
+                                {text: 'Medium', value: 'medium'},
+                                {text: 'Large', value: 'large'}
+                            ]
+                        },
+                        //description
+                        {
+                            type: 'listbox',
+                            name: 'embedDesc',
+                            label: 'Description',
+                            value: embedDesc,
+                            'values': [
+                                {text: 'Hide', value: 'hide'},
+                                {text: 'Show', value: 'show'}
+                            ]
+                        }
+                    ],
+                    onsubmit: function( e ) { //when the ok button is clicked
+
+
+                        if(typeof e.data.embedUrl != 'undefined' && e.data.embedUrl.length) {
+                            var url = e.data.embedUrl;
+
+                            var desc = '';
+                            if(typeof e.data.embedDesc != 'undefined' && e.data.embedDesc.length){
+                                desc = e.data.embedDesc;
+                            } else {
+                                desc = 'false';
+                            }
+
+                            var algin = '';
+                            if(typeof e.data.embedAlign != 'undefined' && e.data.embedAlign.length){
+                                align = e.data.embedAlign;
+                            } else {
+                                align = 'center';
+                            }
+
+                            var size = '';
+                            if(typeof e.data.embedSize != 'undefined' && e.data.embedSize.length){
+                                size = e.data.embedSize;
+                            } else {
+                                size = 'small';
+                            }
+
+                            var shortcode_str = '[es-embed url="'+url+'" align="'+align+'" size="'+size+'" description="'+desc+'"]';
+
+                            //insert shortcode to TinyMCE
+                            editor.insertContent( shortcode_str);
+                        } else {
+                            window.alert("Invalid URL. Please try again.");
+                        }
+                    }
+                });
+
+
             });
         },
         createControl : function(n, cm) {
             return null;
-        },
+        }
     });
     /* Start the buttons */
     tinymce.PluginManager.add( 'es_button_script', tinymce.plugins.EsButtons );
+
 })();
